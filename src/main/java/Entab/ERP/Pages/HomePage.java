e053f86588316a658e4b9ed617510f7015640cef
Zahid <zahid.iqbal@presto.co.in>
2025-11-17 18:06:05 +0530

    Home page changes

--- src/main/java/Entab/ERP/Pages/HomePage.java
+++ src/main/java/Entab/ERP/Pages/HomePage.java
@@ -17,19 +17,67 @@ public class HomePage extends CommonComponents {
 	}
 	
 	@FindBy(xpath = "//span[@id='teacherName']")
-	WebElement profileIcon;
+	WebElement userProfile;
+	
+	public String loginUserName() throws InterruptedException
+	{
+		threadSleep(2000);
+		String userName = userProfile.getText();
+		System.out.println("Login username is : "+userName);
+		return userName;
+	}
+	
 	@FindBy(xpath = "//a[@class='logout']")
 	WebElement logout;
 	
 	public void logOut() throws InterruptedException
 	{
 		threadSleep(2000);
-		profileIcon.click();
+		userProfile.click();
 		System.out.println("User is logged out successfully.");
 		threadSleep(2000);
 		hoverText(driver, logout);
 		driver.close();
 		System.out.println("Closed the browser.");
 	}
+	
+	@FindBy(xpath = "//a[@class='profile']")
+	WebElement profile;
+	
+	public String profileText()
+	{
+		hoverText(driver,userProfile);
+		String prText = profile.getText();
+		System.out.println(prText);
+		return prText;
+	}
+	
+	@FindBy(xpath = "//a[contains(., 'Change Password')]")
+	WebElement passText;
+	
+	public String changePasswordText() throws InterruptedException
+	{
+		threadSleep(2000);
+		hoverText(driver,userProfile);
+		//userProfile.click();
+		threadSleep(2000);
+		String cpText = passText.getText();
+		System.out.println(cpText);
+		return cpText;
+	}
+	
+	@FindBy(xpath="//a[@class='logout']")
+	WebElement logoutText;
+	
+	public String logOutText() throws InterruptedException
+	{
+		threadSleep(2000);
+		hoverText(driver,userProfile);
+		threadSleep(3000);
+		String logText = logoutText.getText();
+		System.out.println(logText);
+		return logText;
+	}
+	
 
 }
---
+++ src/test/java/Entab/ERP/Tests/HomePageVerification.java
@@ -0,0 +1,48 @@
+package Entab.ERP.Tests;
+import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import Entab.ERP.Pages.HomePage;
+import Entab.ERP.Pages.LoginPage;
+import Entab.ERP.TestComponents.BaseTest;
+
+public class HomePageVerification extends BaseTest
+{
+	String name = "ZAHID IQBAL";
+	
+	@Test (priority=1)
+	public void verifyLoginUserName() throws IOException, InterruptedException
+	{
+		loginPage.verifyLoginPage();
+		loginPage.loginApplication();		
+		HomePage homePage = new HomePage(driver);
+		String actualName = homePage.loginUserName();		
+		Assert.assertEquals(actualName, name);
+	}
+	
+	@Test(priority=2)
+	public void verifyProfile() throws IOException, InterruptedException
+	{
+		HomePage homePage = new HomePage(driver);
+		String actualName = homePage.profileText();
+		Assert.assertEquals(actualName, "Profile");
+	}
+	
+	@Test(priority=3)
+	public void verifyChangePassword() throws IOException, InterruptedException
+	{
+		HomePage homePage = new HomePage(driver);		
+		String actualName = homePage.changePasswordText();	
+		Assert.assertEquals(actualName, "Change Password");
+	}
+
+	@Test(priority=4)
+	public void verifyLogoutText() throws IOException, InterruptedException
+	{
+		HomePage homePage = new HomePage(driver);
+		String actualName = homePage.logOutText();	
+		Assert.assertEquals(actualName, "Logout");
+	}
+}
